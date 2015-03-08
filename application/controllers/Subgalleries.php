c4ded2a4cf0636e7267b72fab6c4f343f08bcca7
chodolak <chodolak@gmail.com>
2015-03-07 19:41:38 -0800

    Created subgalleries controller to replace the four other gallery controllers.

---
+++ application/controllers/Subgalleries.php
@@ -0,0 +1,55 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Subgalleries extends Application {
+    
+    public function people(){
+        $this->data['pagebody'] = 'people';
+        $this->render();
+    }
+    
+    public function blackwhite(){
+        $this->data['pagebody'] = 'blackwhite';
+        $this->render();
+    }
+    
+    public function places(){
+        $this->data['pagebody'] = 'places';
+        $this->render();
+    }
+    
+    public function nature(){
+        //Grabs photo information from database and puts it into array.
+        $this->db->where('category', 'Nature');
+        $query = $this->db->get("photos");
+        $pictures = $query->result_array();
+
+        //Parses the database information into a cell array.
+        foreach ($pictures as $picture) {
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        }
+
+        //Sets up and puts the picture information into a link.
+        $this->load->library('table');
+        $parms = array(
+            'table_open' => '<a class="gallery">',
+            "cell_start" => '<td class="oneimage">',
+            "cell_alt_start" => '<td class="oneimage">'
+            );
+        $this->table->set_template($parms);
+
+        $rows = $this->table->make_columns($cells, 6);
+
+        //Generates pictures in the location.
+        $this->data['thetable'] = $this->table->generate($rows);
+
+        //Renders body.
+        $this->data['pagebody'] = 'nature';
+        $this->render();
+    }
+}
